1b9192413d87e1e2ab08361d453a638dbfdf9db1
Martin Staadecker <machstg@gmail.com>
2024-05-12 18:57:38 +0200

    Improved file writing

--- src/pyoframe/constants.py
+++ src/pyoframe/constants.py
@@ -50,7 +50,8 @@ class Config(metaclass=_ConfigMeta):
     disable_unmatched_checks: bool = False
     print_float_precision: Optional[int] = 5
     print_uses_variable_names: bool = True
-    print_max_set_elements: int = 100  # When printing a Set to console, how many elements to display before elipsing.
+    # Number of elements to show when printing a set to the console (additional elements are replaced with ...)
+    print_max_set_elements: int = 50
 
     @classmethod
     def reset_defaults(cls):
