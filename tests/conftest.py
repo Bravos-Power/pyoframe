b6cac04ce09520417f0301c9491f34d041992013
Martin <machstg@gmail.com>
2025-03-16 15:15:57 -0400

    Improve testing and examples (#112)
    * Rename files
    
    * Reorganize testing
    
    * Switch to line-by-line comparison
    
    * Debug failing tests
    
    * Make git case sensitive

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import pytest
 
 import pyoframe as pf
+from pyoframe.constants import SUPPORTED_SOLVERS
 
 
-@pytest.fixture(params=["gurobi", "highs"])
+@pytest.fixture(params=SUPPORTED_SOLVERS)
 def solver(request):
     return request.param
 
@@ -11,3 +12,12 @@ def solver(request):
 @pytest.fixture(autouse=True)
 def _apply_solver(solver):
     pf.Config.default_solver = solver
+
+
+@pytest.fixture(params=[True, False])
+def use_var_names(request, solver):
+    if solver == "highs" and request.param:
+        pytest.skip(
+            "Highs does not support variable names. See https://github.com/Bravos-Power/pyoframe/issues/102#issuecomment-2727521430"
+        )
+    return request.param
---
+++ tests/examples/diet_problem/results/solution-gurobi-pretty.sol
@@ -0,0 +1,11 @@
+# Objective value = 1.2060249999999998e+01
+ONE 1
+Buy[hamburger] 5.5526315789473712e-01
+Buy[chicken] 0
+Buy[hot_dog] 0
+Buy[fries] 0
+Buy[macaroni] 0
+Buy[pizza] 0
+Buy[salad] 0
+Buy[milk] 6.8
+Buy[ice_cream] 2.9092105263157881e+00
