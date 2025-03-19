bce851f75632ecc5b9f8d5971af9734b48dd1841
Martin <machstg@gmail.com>
2025-03-19 11:47:30 -0400

    Add support for writing .lp and .sol files in HiGHS (#114)
    * Add support for writing .lp and .sol files in HiGHS
    
    * Fix testing
    
    * Fix testing v2

---
+++ tests/examples/facility_problem/results/solution-highs-machine.sol
@@ -0,0 +1,50 @@
+Model status
+Optimal
+
+# Primal solution values
+Feasible
+Objective 210500
+# Columns 26
+ONE 1
+col_ekk_0 1
+col_ekk_1 1
+col_ekk_2 0
+col_ekk_3 1
+col_ekk_4 1
+col_ekk_5 0
+col_ekk_6 14
+col_ekk_7 0
+col_ekk_8 1
+col_ekk_9 0
+col_ekk_10 0
+col_ekk_11 0
+col_ekk_12 0
+col_ekk_13 18
+col_ekk_14 0
+col_ekk_15 14
+col_ekk_16 0
+col_ekk_17 0
+col_ekk_18 0
+col_ekk_19 0
+col_ekk_20 6
+col_ekk_21 8
+col_ekk_22 0
+col_ekk_23 0
+col_ekk_24 6
+# Rows 9
+R0 0
+R1 0
+R2 0
+R3 0
+R4 -12
+R5 0
+R6 0
+R7 0
+R8 0
+
+# Dual solution values
+None
+
+# Basis
+HiGHS v1
+None
