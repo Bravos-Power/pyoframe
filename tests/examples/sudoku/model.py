01249547d9c9e44b293e8c9105d2b4de1148c286
Martin Staadecker <machstg@gmail.com>
2025-03-24 22:47:38 -0400

    Simplify code

--- tests/examples/sudoku/model.py
+++ tests/examples/sudoku/model.py
@@ -5,35 +5,33 @@ from pathlib import Path
 import polars as pl
 
 import pyoframe as pf
-from pyoframe import sum
+from pyoframe import sum_by
 
 
 def solve_model(use_var_names=True):
-    init_values = pl.read_csv(
-        Path(__file__).parent / "input_data" / "initial_numbers.csv"
+    init_values = pf.Set(
+        pl.read_csv(Path(__file__).parent / "input_data" / "initial_numbers.csv")
     )
-    init_values = init_values.with_columns(init_values=1)
 
     digits = pl.int_range(1, 10, eager=True)
 
-    cube9x9x9 = (
-        pl.DataFrame({"row": digits})
-        .join(pl.DataFrame({"column": digits}), how="cross")
-        .join(pl.DataFrame({"digit": digits}), how="cross")
+    cells = pl.DataFrame({"row": digits}).join(
+        pl.DataFrame({"column": digits}), how="cross"
     )
-    cube9x9x9 = cube9x9x9.with_columns(
+    cell_to_box = cells.with_columns(
         box=((pl.col("row") - 1) // 3 + 1) * 10 + ((pl.col("column") - 1) // 3 + 1)
     )
+    cell_options = cells.join(pl.DataFrame({"digit": digits}), how="cross")
 
-    m = pf.Model("sudoku_binary", use_var_names=use_var_names)
-    m.Y = pf.Variable(cube9x9x9, vtype=pf.VType.BINARY)
+    m = pf.Model(use_var_names=use_var_names)
+    m.Y = pf.Variable(cell_options, vtype=pf.VType.BINARY)
 
-    m.given_values = m.Y.drop_unmatched() == init_values.to_expr().add_dim("box")
+    m.given_values = m.Y.drop_unmatched() == init_values
 
-    m.just_one_digit_is_set_to_rxc = sum(["digit", "box"], m.Y) == 1
-    m.each_row_all_digits = sum(["column", "box"], m.Y) == 1
-    m.each_column_all_digits = sum(["row", "box"], m.Y) == 1
-    m.each_box_has_all_digits = sum(["row", "column"], m.Y) == 1
+    m.one_per_row = sum_by(["digit", "row"], m.Y) == 1
+    m.one_per_column = sum_by(["digit", "column"], m.Y) == 1
+    m.one_per_box = sum_by(["digit", "box"], m.Y.map(cell_to_box)) == 1
+    m.one_per_cell = sum_by(["row", "column"], m.Y) == 1
 
     if m.solver_name == "gurobi":
         m.params.Method = 2
@@ -44,4 +42,4 @@ def solve_model(use_var_names=True):
 
 
 if __name__ == "__main__":
-    main()
+    solve_model()
