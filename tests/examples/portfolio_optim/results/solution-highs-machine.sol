cf0ddf67bc920e98c9cc85d313affb3545fa5a7b
Martin Staadecker <machstg@gmail.com>
2025-09-28 17:07:55 -0400

    Add test files

---
+++ tests/examples/portfolio_optim/results/solution-highs-machine.sol
@@ -0,0 +1,37 @@
+Model status
+Optimal
+
+# Primal solution values
+Feasible
+Objective 0.0195035306841
+# Columns 6
+ONE 1
+col_ekk_0 0.3606701950577
+col_ekk_1 0.1472124715774
+col_ekk_2 0.2093379268716
+col_ekk_3 0.1453075718898
+col_ekk_4 0.1374718346036
+# Rows 2
+R0 0
+R1 0
+
+# Dual solution values
+Feasible
+# Columns 6
+ONE 0.0390071849272
+col_ekk_0 0
+col_ekk_1 0
+col_ekk_2 0
+col_ekk_3 0
+col_ekk_4 0
+# Rows 2
+R0 -0.0191050781875
+R1 0.5811216311471
+
+# Basis
+HiGHS v1
+Valid
+# Columns 6
+0 1 4 4 4 1 
+# Rows 2
+2 0 
---
+++ tests/examples/portfolio_optim/results/solution-highs-pretty.sol
@@ -0,0 +1,37 @@
+Model status
+Optimal
+
+# Primal solution values
+Feasible
+Objective 0.0195035306841
+# Columns 6
+ONE 1
+weight[A] 0.3606701950577
+weight[B] 0.1472124715774
+weight[C] 0.2093379268716
+weight[D] 0.1453075718898
+weight[E] 0.1374718346036
+# Rows 2
+con_weights_sum 0
+con_min_return 0
+
+# Dual solution values
+Feasible
+# Columns 6
+ONE 0.0390071849272
+weight[A] 0
+weight[B] 0
+weight[C] 0
+weight[D] 0
+weight[E] 0
+# Rows 2
+con_weights_sum -0.0191050781875
+con_min_return 0.5811216311471
+
+# Basis
+HiGHS v1
+Valid
+# Columns 6
+0 1 4 4 4 1 
+# Rows 2
+2 0 
