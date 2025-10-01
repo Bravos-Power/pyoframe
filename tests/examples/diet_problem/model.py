401b181c5926704f533dc210c66f80aee8f18eab
Martin Staadecker <machstg@gmail.com>
2025-10-01 20:03:27 +0000

    Revert previous changes

--- tests/examples/diet_problem/model.py
+++ tests/examples/diet_problem/model.py
@@ -10,14 +10,14 @@ import pyoframe as pf
 _input_dir = Path(os.path.dirname(os.path.realpath(__file__))) / "input_data"
 
 
-def solve_model(use_var_names=False, solver=None):
+def solve_model(use_var_names):
     food = pl.read_csv(_input_dir / "foods.csv")
     nutrients = pl.read_csv(_input_dir / "nutrients.csv")
     min_nutrient = nutrients.select(["category", "min"]).to_expr()
     max_nutrient = nutrients.select(["category", "max"]).to_expr()
     food_nutrients = pl.read_csv(_input_dir / "foods_to_nutrients.csv").to_expr()
 
-    m = pf.Model(solver, solver_uses_variable_names=use_var_names)
+    m = pf.Model(solver_uses_variable_names=use_var_names)
     m.Buy = pf.Variable(food["food"], lb=0, ub=food[["food", "stock"]])
 
     m.min_nutrients = min_nutrient <= (m.Buy * food_nutrients).sum("food").drop_extras()
@@ -31,4 +31,4 @@ def solve_model(use_var_names=False, solver=None):
 
 
 if __name__ == "__main__":
-    solve_model(solver="ipopt")
+    solve_model()
