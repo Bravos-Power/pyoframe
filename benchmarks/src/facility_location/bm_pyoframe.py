b294433c7d2f5927c3e7c840b5d68db4973a0541
Martin Staadecker <machstg@gmail.com>
2026-06-07 14:45:14 +0000

    Change model to m

--- benchmarks/src/facility_location/bm_pyoframe.py
+++ benchmarks/src/facility_location/bm_pyoframe.py
@@ -8,45 +8,42 @@ from pyoframe import Model, Param, Set, Variable
 class Bench(Benchmark):
     def build(self, **kwargs):
         G = F = self.size
-        assert G is not None
 
         grid = range(G + 1)
 
         customer_position_x = Param({"x": grid, "x_pos": [step / G for step in grid]})
         customer_position_y = Param({"y": grid, "y_pos": [step / G for step in grid]})
 
-        model = Model()
-        model.facilities = Set(f=range(1, F + 1))
-        model.grid = Set(x=grid, y=grid)
-        model.axis = Set(d=[1, 2])
+        m = Model()
+        m.facilities = Set(f=range(1, F + 1))
+        m.grid = Set(x=grid, y=grid)
+        m.axis = Set(d=[1, 2])
 
-        model.facility_position = Variable(model.facilities, model.axis, lb=0, ub=1)
+        m.facility_position = Variable(m.facilities, m.axis, lb=0, ub=1)
 
-        model.max_distance = Variable(lb=0)
+        m.max_dist = Variable(lb=0)
 
-        model.is_closest = Variable(model.grid, model.facilities, vtype="binary")
-        model.con_only_one_closest = model.is_closest.sum("f") == 1
-        model.dist_xy = Variable(model.grid, model.facilities, model.axis)
-        model.con_dist_x = model.dist_xy.pick(d=1) == (
-            customer_position_x.over("f") - model.facility_position.pick(d=1).over("x")
+        m.is_closest = Variable(m.grid, m.facilities, vtype="binary")
+        m.con_only_one_closest = m.is_closest.sum("f") == 1
+        m.dist_xy = Variable(m.grid, m.facilities, m.axis)
+        m.con_dist_x = m.dist_xy.pick(d=1) == (
+            customer_position_x.over("f") - m.facility_position.pick(d=1).over("x")
         ).over("y")
-        model.con_dist_y = model.dist_xy.pick(d=2) == (
-            customer_position_y.over("f") - model.facility_position.pick(d=2).over("y")
+        m.con_dist_y = m.dist_xy.pick(d=2) == (
+            customer_position_y.over("f") - m.facility_position.pick(d=2).over("y")
         ).over("x")
-        model.dist = Variable(model.grid, model.facilities, lb=0)
-        model.con_dist = (
-            model.dist**2 == model.dist_xy.pick(d=1) ** 2 + model.dist_xy.pick(d=2) ** 2
-        )
+        m.dist = Variable(m.grid, m.facilities, lb=0)
+        m.con_dist = m.dist**2 == m.dist_xy.pick(d=1) ** 2 + m.dist_xy.pick(d=2) ** 2
 
         # Twice the max distance which ensures that when is_closest is 0, the constraint is not binding.
         M = 2 * 1.414
-        model.con_max_distance = model.max_distance.over(
-            "x", "y", "f"
-        ) >= model.dist - M * (1 - model.is_closest)
+        m.con_max_dist = m.max_dist.over("x", "y", "f") >= m.dist - M * (
+            1 - m.is_closest
+        )
 
-        model.minimize = model.max_distance
+        m.minimize = m.max_dist
 
-        return model
+        return m
 
 
 if __name__ == "__main__":
